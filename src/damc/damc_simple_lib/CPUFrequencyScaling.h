190182399be52769116a79edf7ba65a44957b4a8
Alexis Murzeau <amubtdx@gmail.com>
2025-03-30 23:33:12 +0200

    stm32n6: allow fine control of clocks from OSC

--- src/damc/damc_simple_lib/CPUFrequencyScaling.h
+++ src/damc/damc_simple_lib/CPUFrequencyScaling.h
@@ -16,14 +16,51 @@ public:
 	void updateCpuUsage();
 
 protected:
-	void setAHBDivider(uint32_t divider);
+	enum class CpuFreqAdjustement {
+		ResetToMax,
+		IncreaseSpeed,
+		DecreaseSpeed,
+	};
+	void adjustCpuFreq(CpuFreqAdjustement adjustment);
 	static void onFrequencyChanged(uv_async_t* handle);
+
+#if defined(STM32F723xx)
+	void setAHBDivider(uint32_t divider);
+#elif defined(STM32N657xx)
+	void setRawCPUDivider(uint32_t divider);
+	void setRawAXIDivider(uint32_t divider);
+	void setRawAHBDivider(uint32_t divider);
+	void setRawAPBDivider(uint32_t index, uint32_t divider);
+	void setRawTimerDivider(uint32_t index, uint32_t divider);
+#endif
 
 private:
 	OscRoot* oscRoot;
 	OscVariable<bool> oscManualControl;
-	OscReadOnlyVariable<int32_t> oscCurrentFrequency;
+
+#if defined(STM32F723xx)
+	OscReadOnlyVariable<int32_t> oscCpuFrequency;
 	OscVariable<int32_t> oscCpuDivider;
+#elif defined(STM32N657xx)
+	OscReadOnlyVariable<int32_t> oscPllFrequency;
+	OscReadOnlyVariable<int32_t> oscCpuFrequency;
+	OscReadOnlyVariable<int32_t> oscAXIFrequency;
+	OscReadOnlyVariable<int32_t> oscAHBFrequency;
+	OscReadOnlyVariable<int32_t> oscAPB1Frequency;
+	OscReadOnlyVariable<int32_t> oscAPB2Frequency;
+	OscReadOnlyVariable<int32_t> oscAPB4Frequency;
+	OscReadOnlyVariable<int32_t> oscAPB5Frequency;
+	OscReadOnlyVariable<int32_t> oscTimerFrequency;
+
+	OscVariable<int32_t> oscCpuDivider;
+	OscVariable<int32_t> oscAXIDivider;
+	OscVariable<int32_t> oscAHBDivider;
+	OscVariable<int32_t> oscAPB1Divider;
+	OscVariable<int32_t> oscAPB2Divider;
+	OscVariable<int32_t> oscAPB4Divider;
+	OscVariable<int32_t> oscAPB5Divider;
+	OscVariable<int32_t> oscTimerDivider;
+#endif
 
 	uint32_t current_ahb_divider;
 	bool recent_ahb_divider_change;
