ab83bc8de6bf3fe010230b4c57d1a95eb1b5ba9b
Alexis Murzeau <amubtdx@gmail.com>
2025-05-26 16:39:54 +0200

    stm32f7: reduce SAI GPIO speed to reduce noise on wires

--- src/stm32f723disco/Core/Src/main.c
+++ src/stm32f723disco/Core/Src/main.c
@@ -447,7 +447,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_3 | GPIO_PIN_6 | GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF6_SAI1;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
