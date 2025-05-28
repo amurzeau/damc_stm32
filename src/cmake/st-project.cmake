17659669833673aa4d69be094d96017a145f8657
Alexis Murzeau <amubtdx@gmail.com>
2025-05-28 23:32:06 +0200

    stm32n6: build both FSBL and Appli with CMake

--- src/cmake/st-project.cmake
+++ src/cmake/st-project.cmake
@@ -5,23 +5,17 @@ function(add_st_target_properties TARGET_NAME)
 	add_custom_command(
 		TARGET ${TARGET_NAME} POST_BUILD
 		COMMAND ${CMAKE_OBJCOPY} -O ihex
-		$<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.hex
+			$<TARGET_FILE:${TARGET_NAME}> $<TARGET_FILE_DIR:${TARGET_NAME}>/${TARGET_NAME}.hex
 	)
 
 	add_custom_command(
 		TARGET ${TARGET_NAME} POST_BUILD
 		COMMAND ${CMAKE_OBJCOPY} -O binary
-		$<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.bin
+			$<TARGET_FILE:${TARGET_NAME}> $<TARGET_FILE_DIR:${TARGET_NAME}>/${TARGET_NAME}.bin
 	)
 
 	add_custom_command(
 		TARGET ${TARGET_NAME} POST_BUILD
-		COMMAND ${CMAKE_OBJDUMP} -h -S $<TARGET_FILE:${TARGET_NAME}> > ${TARGET_NAME}.list
+		COMMAND ${CMAKE_OBJDUMP} -h -S $<TARGET_FILE:${TARGET_NAME}> > $<TARGET_FILE_DIR:${TARGET_NAME}>/${TARGET_NAME}.list
 	)
-	if(STM32_SIGNINGTOOL_CLI)
-		add_custom_command(
-			TARGET ${TARGET_NAME} POST_BUILD
-			COMMAND ${STM32_SIGNINGTOOL_CLI} --silent --binary-image ${TARGET_NAME}.bin --no-keys --option-flags 0x80000000 --type fsbl --output ${TARGET_NAME}-trusted.bin --header-version 2.3 --dump-header ${TARGET_NAME}-trusted.bin
-		)
-	endif()
 endfunction()
